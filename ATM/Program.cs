d7a4f9bb60b0053a99bf928baecd976fdce0ba6f
Joga <jose.gabriel.a12@gmail.com>
2023-04-16 17:10:10 -0500

    Initial implementation of CardHolder class

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class cardHolder
+{
+    String firstName;
+    String lastName;
+    int pin;
+    String cardNum;
+    double balance;
+
+public cardHolder(string firstName, string lastName, int pin, string cardNum, double balance) 
+      
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.pin = pin;
+        this.cardNum = cardNum;
+        this.balance = balance;
+   }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;        
+    }
+    
+    public int getPin()
+    {
+            return pin;
+    }
+
+    public String getCardNum() {
+        return cardNum;    
+    }
+
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    public void setPin(int newPin)
+    {
+        pin = newPin;
+    }
+
+    public  void    setNum(String newCardNum)
+    {
+        cardNum = newCardNum;
+    }
+
+    public void setFirstName(String newFirstName)
+    {
+        firstName = newFirstName;
+    }
+
+    public void setLastName(String newLastName)
+    { lastName = newLastName; }
+     
+    public void setBalance(double newBalance)
+    { balance = newBalance; }
+
+
+
+
+}
